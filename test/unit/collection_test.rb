5ba97d1996ff3745cb01e800d366a0c1192daf60
Dmitri Zagidulin <dzagidulin@gmail.com>
2013-12-03 17:18:16 -0500

    Red - a collection requires a name

--- test/unit/collection_test.rb
+++ test/unit/collection_test.rb
@@ -1,6 +1,13 @@
 require 'helper'
 
 describe "a RiakJson Collection" do
+  context "when created" do
+    it "requires a name" do
+      client = test_client  # in helper.rb
+      lambda { client.collection() }.must_raise ArgumentError
+    end
+  end
+  
   it "is initialized" do
     col = RiakJson::Collection.new
   end
