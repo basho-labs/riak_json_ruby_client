8b8c2ed2dc4c52a9b90d8de80c371536c7df56b3
Dmitri Zagidulin <dzagidulin@gmail.com>
2013-12-03 22:18:55 -0500

    Extract collection integration tests to separate file

--- test/integration/client_integration_test.rb
+++ test/integration/client_integration_test.rb
@@ -12,20 +12,9 @@ describe "RiakJson Ruby Client" do
     it "raises an ArgumentError on send_request with invalid HTTP method"
   end
   
-  context "performs read/write/delete on a Document" do
-    it "issues PUT requests to write a new document"
-    it "issues POST requests to update an existing document"
-    it "issues GET requests to read an existing document"
-  end
-  
   context "performs document Schema administration" do
     it "issues PUT requests to create a new schema"
     it "issues GET requests to read an existing schema from RiakJson/Solr"
     it "issues DELETE requests to remove a schema"
-  end
-  
-  context "performs queries" do
-    it "retrieves a single document with find_one()"
-    it "retrieves many documents with find()"
   end
 end
