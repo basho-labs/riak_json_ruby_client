da6c6c9fcebbe71eff43f0281c22b14e77375248
Dmitri Zagidulin <dzagidulin@gmail.com>
2013-12-04 11:28:58 -0500

    Red - failing test for collection.insert

--- test/unit/collection_test.rb
+++ test/unit/collection_test.rb
@@ -27,4 +27,21 @@ describe "a RiakJson Collection" do
       collection.client.must_be_kind_of(RiakJson::Client)
     end
   end
+  
+  context "uses a RiakJson Client to" do
+    it "insert a JSON document into a collection/key" do
+      client = test_client
+      collection_name = 'test_collection'
+      collection = client.collection(collection_name)  # create a new collection object
+
+      test_key = 'document-key-123'
+      document = { :key => test_key, :field_one => '123', :field_two => 'abc' }.to_json
+
+      # Test that a collection.insert(doc) results in a call to client.insert_json_object
+      collection.client = MiniTest::Mock.new
+      collection.client.expect :insert_json_object, nil, [collection_name, test_key, document]
+      collection.insert(document)
+      collection.client.verify
+    end
+  end
 end
