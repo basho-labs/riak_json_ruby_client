bab8d77a7463fd3eb09164ef9f0a421804c713b8
Dmitri Zagidulin <dzagidulin@gmail.com>
2013-12-04 19:17:00 -0500

    Clear remaining client code from 0.0.1

--- lib/riak_json/client.rb
+++ lib/riak_json/client.rb
@@ -77,30 +77,5 @@ module RiakJson
     def update_json_object(collection_name, key, json)
       self.transport.send_request("#{self.base_collection_url}/#{collection_name}/#{key}", :post, json)
     end
-    
-#    def send_request(req_opts)
-#      uri = URI.parse("#{self.options[:host]}#{req_opts[:path]}")
-#      http = Net::HTTP.new(uri.host, uri.port)
-#  
-#      case req_opts[:method]
-#        when :get
-#          req = Net::HTTP::Get.new(uri.request_uri)
-#        when :put
-#          req = Net::HTTP::Put.new(uri.request_uri)
-#          req.content_type = 'application/json'
-#          req.body = req_opts[:data]
-#        when :post
-#          req = Net::HTTP::Post.new(uri.request_uri)
-#          req["content_type"] = 'application/json'
-#          req.body = req_opts[:data]
-#        when :delete
-#          req = Net::HTTP::Delete.new(uri.request_uri)
-#        else
-#          raise ArgumentError, 'Not a valid :method'
-#      end
-#  
-#      res = http.request(req)
-#      res.body
-#    end
   end
 end
